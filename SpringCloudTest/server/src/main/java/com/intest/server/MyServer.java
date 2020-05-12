c7f2f47e8427dc5b033b41499bc325fc814bcbf7
zhangam <zhangam@intest.cn>
2020-05-12 13:18:38 +0800

    springboot项目外部tomcat打包部署

--- SpringCloudTest/server/src/main/java/com/intest/server/MyServer.java
+++ SpringCloudTest/server/src/main/java/com/intest/server/MyServer.java
@@ -1,16 +1,26 @@
 package com.intest.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 //eureka服务端
 @EnableEurekaServer
-public class MyServer {
+public class MyServer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MyServer.class);
+    }
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(MyServer.class).run(args);
+        SpringApplication.run(MyServer.class, args);
+//        SpringApplication application = new SpringApplication(MyServer.class);
+//        application.setBannerMode(Banner.Mode.OFF);
+//        application.run(args);
     }
 
 }
