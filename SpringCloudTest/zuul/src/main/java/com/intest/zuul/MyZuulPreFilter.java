9a200e733127a3c70ad86c2f49def6f6dff810a3
zam <598974027@qq.com>
2021-04-01 15:30:50 +0800

    zuul四种类型过滤器

--- SpringCloudTest/zuul/src/main/java/com/intest/zuul/MyZuulPreFilter.java
+++ SpringCloudTest/zuul/src/main/java/com/intest/zuul/MyZuulPreFilter.java
@@ -2,8 +2,11 @@ package com.intest.zuul;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 功能描述: 自定义过滤器
@@ -14,6 +17,16 @@ import javax.servlet.http.HttpServletRequest;
  **/
 public class MyZuulPreFilter extends ZuulFilter {
 
+    /**
+     * 4种不同生命周期的过滤器类型
+     * pre route error post
+     * pre过滤器： 在请求被路由之前调用。Zuul请求微服务之前。比如请求身份验证，选择微服务实例，记录调试信息等
+     * route过滤器： 负责转发请求到微服务。原始请求在此构建，并使用Apache HttpClient或Netflix Ribbon发送原始请求
+     * error过滤器： 在处理请求发生错误时被调用
+     * post过滤器： 在route和error过滤器之后被调用。可以在响应添加标准HTTP Header、收集统计信息和指标，以及将响应发送给客户端等
+     *
+     * @return
+     */
     @Override
     public String filterType() {
         return "pre";
@@ -31,8 +44,18 @@ public class MyZuulPreFilter extends ZuulFilter {
 
     @Override
     public Object run() {
-        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
-        System.out.println("请求的URI:" + request.getRequestURI());
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        String token = request.getParameter("token");
+        if (token == null || token.isEmpty() || !token.equals("123456")) {
+            //不对该请求进行路由，不会将请求转发到后端
+            currentContext.setSendZuulResponse(false);
+            //设置错误状态码
+            currentContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+            //设置响应体
+            currentContext.setResponseBody("权限不足.....");
+            currentContext.getResponse().setContentType("text/html;charset=UTF-8");
+        }
         return null;
     }
 
